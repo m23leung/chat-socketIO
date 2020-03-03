2a98d1b588267a305f3d4e5db0d1ce46a28810bb
Mark Leung <mark.leung89@gmail.com>
2020-03-03 11:20:06 +1100

    Initial chat sample app

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    io.emit('chat message', 'a user connected');
+    //socket.broadcast.emit('hi');
+    
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg); 
+        io.emit('chat message', msg);
+    });
+
+    
+    socket.on('disconnect', function() {
+        io.emit('chat message', 'a user disconnected');
+    });
+    
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
